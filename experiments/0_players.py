5e166bd605699655478918f6a7c51733908fc69a
Hugo Richard <h.richard@criteo.com>
2023-01-19 13:16:33 +0000

    update

--- experiments/0_players.py
+++ experiments/0_players.py
@@ -13,7 +13,7 @@ M = 3  # Number of players
 mu = np.array([0.99, 0.01])  # Reward of each arm
 K = len(mu)  # Number of arms
 p = 0.1  # Probability that a player is active at each round
-T = int(1e6)  # Number of rounds
+T = int(1e4)  # Number of rounds
 
 verbose = True
 

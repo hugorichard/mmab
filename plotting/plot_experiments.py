271495df32ee322c9decc0f3d5b83a8e6802df3f
Hugo Richard <research.hugo.richard@gmail.com>
2023-01-20 10:06:36 +0100

    update plotting utilities

---
+++ plotting/plot_experiments.py
@@ -0,0 +1,54 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+names = ["OPT", "ETC", "UCB", "Greedy"]
+
+experiments = ["0_players", "no_elimination"]
+
+rc = {
+    "pdf.fonttype": 42,
+    "text.usetex": True,
+    "font.size": 16,
+    "xtick.labelsize": 16,
+    "ytick.labelsize": 16,
+    "text.usetex": True,
+    "font.family": "serif",
+}
+plt.rcParams.update(rc)
+
+for experiment in experiments:
+    rss_median = np.load("../data/" + experiment + "_rss_median.npy")
+    rss_high = np.load("../data/" + experiment + "_rss_high.npy")
+    rss_low = np.load("../data/" + experiment + "_rss_low.npy")
+
+    _, T = rss_median.shape
+
+    cm = plt.cm.Set1
+    COLORS = {
+        "ETC": cm(2),
+        "CSARA": cm(3),
+        "4 on the best arm": cm(1),
+        "3 on the worse arm": cm(0),
+        "Uniform": cm(4),
+        "UCB": cm(6),
+        "Greedy": cm(7),
+    }
+
+    I = np.arange(T, step=10)
+    plt.figure(figsize=(5, 3))
+    for i, name in enumerate(names):
+        if i == 0:
+            continue
+        plt.plot(
+            np.arange(T),
+            rss_median[i],
+            label=name,
+            color=COLORS[name],
+        )
+        plt.fill_between(
+            np.arange(T)[I], rss_high[i][I], rss_low[i][I], color=COLORS[name], alpha=0.3
+        )
+    plt.ylabel("Cumulated Regret")
+    plt.xlabel("Timestep $t$")
+    plt.legend()
+    plt.savefig("../figures/" + experiment + ".pdf", bbox_inches="tight")
